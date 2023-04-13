59d2a404029cd241e307c60cea22e6f93c926e04
chetna Bagwan <80641491+chetnabagwan@users.noreply.github.com>
2023-04-13 14:17:57 +0530

    Update oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
 class Solution{
-public static voi main}
+public static voi main(String[] args){
+  Scanner sc=new Scanner(System.in);
+  int n =sc.nextInt();
+  if(n%2==0){
+    System.out.println("Even number");
+  }
+  else
+    System.out.println("Odd number")
+  
+}
