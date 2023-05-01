f4a0e6b9861d87451ae82a5a07b9288508453ccc
chetna Bagwan <80641491+chetnabagwan@users.noreply.github.com>
2023-05-01 23:39:29 +0530

    Update printSubarrays.java

--- printSubarrays.java
+++ printSubarrays.java
@@ -1,5 +1,7 @@
 class Solution{
+  
   public static void printSubarrays(int nums[]){
+    
     for(int i=0;i<nums.length;i++){
       for(int j=i;j<nums.length;j++){
         for (int k=i;k<=j;k++){
