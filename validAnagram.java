6fb72d7b3cd248649fcdafc4170ed1e84b43df9a
chetna Bagwan <80641491+chetnabagwan@users.noreply.github.com>
2023-05-18 23:38:44 +0530

    Update validAnagram.java

--- validAnagram.java
+++ validAnagram.java
@@ -1,3 +1,28 @@
-class Solution{
-public static void main(String args[]){
-}}
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] alparr=new int[26];
+        if (s.length()==t.length()){
+        
+        for(int i=0;i<s.length();i++){
+            alparr[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            alparr[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<alparr.length;i++){
+            if(alparr[i]!=0){
+                return false;
+            }
+        }
+        return true; 
+        }
+        else{
+            return false;
+        }
+  public static void main(String args[]){
+    String s="cat";
+    String t="cab";
+    isAnagram(s,t);
+  
+  }
+}
